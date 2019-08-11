9e3ee1213a3ae62a467847064000ccd57d397ac0
nick-seward <nick@homeslice.ca>
2019-08-11 09:34:52 -0400

    Adding simple templated html page and some kitchen test setup

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2019, Nick, All Rights Reserved.
+
+MyApp 'myapp' do
+    action :install
+end
---
+++ resources/default.rb
@@ -0,0 +1,20 @@
+default_action :install
+
+action :install do
+    public_ip = "public"
+    private_ip = "private"
+
+    # Get the ip addresses from AWS
+
+    template '/index.html' do
+        source 'index.html.erb'
+        variables(
+            publicip: public_ip,
+            privateip: private_ip
+        )
+        owner 'root'
+        group 'root'
+        mode '0644'
+        action :create
+    end
+end
