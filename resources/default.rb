cae5531c548b15c6feed79048436101d52359952
nick-seward <nick@homeslice.ca>
2019-08-13 14:43:02 -0400

    Adding some basic protection against NET exceptions

--- resources/default.rb
+++ resources/default.rb
@@ -4,15 +4,21 @@ action :install do
     ec2_meta_data_url   = "http://169.254.169.254/latest/meta-data/"
     ec2_public_ip_url   = ec2_meta_data_url + "public-ipv4"
     ec2_private_ip_url  = ec2_meta_data_url + "local-ipv4"
-    public_ip           = "--"
-    private_ip          = "--"
+    public_ip           = " --- "
+    private_ip          = " --- "
 
-    # Get the ip addresses from AWS
-    http_call = Chef::HTTP.new(ec2_public_ip_url)
-    public_ip = http_call.request('get', http_call.url)
-
-    http_call = Chef::HTTP.new(ec2_private_ip_url)
-    private_ip = http_call.request('get', http_call.url)
+    # Get the public and private ip addresses from AWS
+    begin
+        http_call = Chef::HTTP.new(ec2_public_ip_url)
+        public_ip = http_call.request('get', http_call.url)
+    rescue
+    end
+        
+    begin
+        http_call = Chef::HTTP.new(ec2_private_ip_url)
+        private_ip = http_call.request('get', http_call.url)
+    rescue 
+    end
 
     template '/usr/share/nginx/html/index.html' do
         source 'index.html.erb'
